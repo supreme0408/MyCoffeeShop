3c3f3b2d230b8c9261c6789779188c3c22b07c0f
Ramanuj <osupreme0408@gmail.com>
2023-06-01 16:49:46 +0530

    interact with smart contract

--- src/App.js
+++ src/App.js
@@ -1,24 +1,190 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react';
+import React from 'react';
+import Header from './Header';
+import Content from './Content';
+
+import 'rsuite/dist/rsuite.min.css';
+import { ChakraProvider,Button} from '@chakra-ui/react'
+import { Navbar, Nav } from 'rsuite';
+const Web3 = require('web3');
+const CoffeeShopABI = require('./abis/CoffeeShop.json');
+const CoffeeTokenABI = require('./abis/CoffeeToken.json');
+const {ethereum} = window;
+
 
 function App() {
+  const [currentAddress, setCurrentAddress] = useState('');
+  const [shopContract,setShopContract] = useState({});
+  const [tokenContract,setTokenContract] = useState({});
+  const [owner,setOwner] = useState('');
+  const [isConnect,setConnected] = useState(0);
+  const[itemids,setitemids] = useState([]);
+  const [allItems, setallItems] = useState([]);
+  const [accbal,setaccbal]= useState(0);
+  const web3 = new Web3(window.ethereum);
+
+  function showLoading(){
+    document.getElementById('text1').innerHTML = "Loading ...";
+  }
+  function hideLoading(){
+    document.getElementById('text1').innerHTML = "Successful txn";
+  }
+  function showSuccessMessage(text2){
+    alert("SUCCESS", text2);
+  }
+  function showErrorMessage(text1){
+    alert(text1);
+  }
+
+  const handleDisconnect = (accounts) => {
+    if (accounts.length === 0) {
+      setCurrentAddress("");
+      setConnected(0);
+    } else {
+      setCurrentAddress(accounts[0]);
+      setConnected(1);
+      getAccountBalance();
+    }
+    window.location.reload();
+  };
+
+  const handleChainChanged = (chainId) => {
+    // If the chain is changed to goerli network, don<t do anything.
+    if (chainId === `0x${Number(11155111).toString(16)}`) return; // chain id is received in hexadecimal
+
+    // chain is changed to any other network, reload the page.
+    // On reload, checkConnection will run due to useEffect.
+    // Inside of that function, we are asking user to switch to goerli network.
+    window.location.reload();
+  };
+
+  const getContract = async()=>{
+    if(ethereum){
+      const CoffeeShopAddress = '0x750070F04A108249d092917a9acC84A5FdeE606B';
+      const CoffeeTokenAddress = '0x6e2841E9a83C6Fa5eeed041aDF766FB64fc8800D';
+      const cs = new web3.eth.Contract(CoffeeShopABI.abi,CoffeeShopAddress);
+      const ct = new web3.eth.Contract(CoffeeTokenABI.abi,CoffeeTokenAddress);
+    
+      setShopContract(cs);
+      setTokenContract(ct);
+
+      setOwner(await cs.methods.getContractOwner());
+
+      const fetchData = async () => {
+      const ids = await cs.methods.getItemIds().call();
+      const itemsArray = await Promise.all(ids.map(async (id) => {
+          const itemData = await cs.methods.getItemInfo(id).call();
+          return { id, ...itemData };
+      }));
+
+      setallItems(itemsArray);
+      setitemids(ids);
+      console.log(ids);
+    }
+    fetchData();
+    }
+  };
+
+  const checkConnection = async ()=>{
+      try{
+        await window.ethereum.request({
+          method:"wallet_switchEthereumChain",
+          params:[{ chainId: `0x${Number(11155111).toString(16)}` }],
+          jsonrpc: '2.0',
+        });
+      } catch(error){
+        console.log(error);
+        // If there was error switching, code 4902 represents that goerli network is not added to metamask, so method below will pre-fill the network details and ask user to add the network.
+        // if (error.code === 4902) {
+        //   addNetwork();
+        // }
+        if (error.code === -32002) {
+          alert("Open Metamask");
+        }
+      }
+      await getContract();
+      await getAccountBalance();
+    // } catch(error){
+    //   console.log(error);
+    };
+
+  const walletConnect = async()=>{
+    try {
+        if (ethereum){
+  
+        const accounts = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setCurrentAddress(accounts[0]);
+        setConnected(1);
+      }
+      } catch (error) {
+        console.log(error);
+      }
+      await getAccountBalance();
+      console.log("Current Address: ",currentAddress);
+    }
+
+    function trimAddr(addr){
+      if(addr!= null){
+      let addr1 = addr.toUpperCase();
+      return ("✅"+addr1.substring(0,4)+"xxxx"+addr1.substring(37));
+      }
+    }
+
+    const getAccountBalance= async()=>{
+      if(isConnect && shopContract !=null){
+        let bal = await shopContract.methods.getUserBalance(currentAddress).call({from:currentAddress});
+        let trimbal = web3.utils.fromWei(bal,"ether");
+        setaccbal(trimbal.toString());
+        console.log("Balance",trimbal.toString());
+      }
+      else{
+        console.log("NOT connected");
+      }
+  
+    };
+
+  useEffect(()=>{
+    if (!window.ethereum){
+      console.log("*****ERROR*****"); 
+      return;
+    }
+    checkConnection();
+    getContract();
+
+    // After connecting wallet
+    // In metamask, you can either change the active account(user), or change the active network (goerli, mumbai, kovan, etc.)
+    ethereum.on("chainChanged", handleChainChanged);
+    ethereum.on("accountsChanged", handleDisconnect);
+    getAccountBalance();
+    // Cleanup of listener on unmount
+    return () => {
+      ethereum.removeListener("chainChanged", handleChainChanged);
+      ethereum.removeListener("accountsChanged", handleDisconnect);
+    };
+
+  },[isConnect,accbal]);
+
   return (
+    <>
+    <ChakraProvider>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/><br/>
+      <Nav pullRight style={{position:'fixed'}}>
+          { !isConnect ?
+          <Button colorScheme='purple' className='btn-pos' onClick={walletConnect}>Connect Wallet</Button>
+          : <React.Fragment>
+            <div className='nav-addr'>{trimAddr(currentAddress)}</div>
+            <div className='nav-addr'>{accbal} CLT</div>
+            </React.Fragment>
+        }
+        </Nav>
+      <Content SC={shopContract} TC={tokenContract} currentAddress={currentAddress} isconnect={isConnect} allItems={allItems} setbal={setaccbal}/>
     </div>
+    </ChakraProvider>
+    </>
   );
 }
 
---
+++ src/Content.js
@@ -0,0 +1,180 @@
+import React, { useState } from "react";
+import {
+    SimpleGrid,
+    Card,
+    Heading,
+    CardBody,
+    Image,
+    Stack,
+    Text,
+    Divider,
+    CardFooter,
+    ButtonGroup,
+    Button,
+    Center
+} from "@chakra-ui/react";
+import './App.css';
+const Web3 = require('web3');
+
+function Content({ SC,TC, currentAddress, isconnect, allItems,setbal }) {
+    const [isbCWEcall, setbCWEcall] = useState(0);
+    const [isbCWCcall, setbCWCcall] = useState(0);
+    //const [allItems, setallItems] = useState([]);
+    const web3 = new Web3(window.ethereum);
+
+    function converttoETH(value) {
+        return web3.utils.fromWei(value, "ether");
+    }
+
+    function showSuccessMessage(text2) {
+        alert("SUCCESS", text2);
+    }
+    function showErrorMessage(text1) {
+        alert(text1);
+    }
+
+    const getAccountBalance= async()=>{
+        if(isconnect && TC !=null){
+          let bal = await SC.methods.getUserBalance(currentAddress).call({from:currentAddress});
+          let trimbal = web3.utils.fromWei(bal,"ether");
+          setbal(trimbal.toString());
+        }
+        else{
+          console.log("NOT connected");
+        }
+      };
+
+
+    const buywithETH = async (id, amount) => {
+        if (isconnect && !isbCWEcall && SC !=null) {
+            try {
+                setbCWEcall(1);
+                //let tokenTogive = Web3.utils.toWei("1", "ether");
+
+                await SC.methods.bCWETH(id).send({ from: currentAddress, value: amount })
+                    .on('receipt', function (receipt) {
+                        // transaction confirmed, hide loading UI and update UI to show success message
+                        showSuccessMessage(receipt);
+                    })
+                    .on('error', function (error, receipt) {
+                        // transaction rejected, hide loading UI and update UI to show error message
+                        showErrorMessage(error.message);
+                    });
+            }
+            catch (error) {
+                setbCWEcall(0);
+                alert(error);
+            }
+            await getAccountBalance();
+            setbCWEcall(0);
+        }
+        else {
+            if (!isconnect) {
+                alert("NO wallet Connected");
+            }
+            else if (isbCWEcall) {
+                alert("Transaction in process");
+            }
+        }
+    };
+
+    const buywithCLT = async (id,amount) => {
+        if (isconnect && !isbCWCcall) {
+            try {
+                setbCWCcall(1);
+                await TC.methods.approve('0x750070F04A108249d092917a9acC84A5FdeE606B',amount).send({from:currentAddress})
+                    .on('transactionHash',async (hash)=>{
+                        await SC.methods.bCWCLT(id).send({ from: currentAddress})
+                        .on('receipt',function(receipt){
+                            alert("SUCCESS");
+                        })
+                        .on('error', function (error, receipt) {
+                            // transaction rejected, hide loading UI and update UI to show error message
+                            alert("FAIL");
+                        });
+                    })
+                    .on('error', function (error, receipt) {
+                        // transaction rejected, hide loading UI and update UI to show error message
+                        showErrorMessage(error.message);
+                    });
+            }
+            catch (error) {
+                setbCWCcall(0);
+                alert(error);
+            }
+            await getAccountBalance();
+            setbCWCcall(0);
+        }
+        else {
+            if (!isconnect) {
+                alert("NO wallet Connected");
+            }
+            else if (isbCWCcall) {
+                alert("Transaction in process");
+            }
+        }
+    };
+
+    const txnanimation = ()=>{
+        if(!isbCWCcall || !isbCWEcall){
+            return(<div id="txn-wait">Wait...</div>);
+        }
+    };
+
+    return (
+        <div id="menu" >
+            {(isbCWCcall||isbCWEcall)?<div id="txnwait">Wait. . .</div>:<div></div> }
+            <h2>Our Menu</h2>
+            <SimpleGrid
+                spacing={14}
+                templateColumns="repeat(auto-fill, minmax(300px, 10fr))"
+            >
+                {allItems.map((item) => (
+                    <React.Fragment key={item.id}>
+                        <Card maxW="sm" align='center'>
+                            <CardBody>
+                                <Image
+                                    src={item['src']}
+                                    boxSize='200px'
+                                    alt="Coffee Image"
+                                    borderRadius="lg"
+                                />
+                                <Stack mt="4" spacing="2">
+                                    <Heading size="md">{item['Name']}</Heading>
+                                    
+                                    <Text color="blue.600" fontSize="2xl">
+                                        {converttoETH(item['ETHPrice'])} ETH
+                                    </Text>
+                                    <Text color="blue.600" fontSize="2xl">
+                                        {converttoETH(item['CLTPrice'])} CLT
+                                    </Text>
+                                </Stack>
+                            </CardBody>
+                            <Divider />
+                            <CardFooter>
+                                <ButtonGroup gap='2' flexDirection={"column"}>
+                                    <Button variant="solid" colorScheme="blue" onClick={async () => buywithETH(item.id, item['ETHPrice'])}>
+                                        Buy now with ETH
+                                    </Button>
+
+                                    <Button variant="solid" colorScheme="blue" onClick={async () => buywithCLT(item.id,item['CLTPrice'])}>
+                                        Buy now with CLT
+                                    </Button>
+
+                                </ButtonGroup>
+                            </CardFooter>
+                        </Card>
+                    </React.Fragment>
+                ))}
+            </SimpleGrid>
+        </div>
+
+    );
+}
+
+export default Content;
+
+//100 Americano 5*10**16 10*10**18 "https://e7.pngegg.com/pngimages/890/365/png-clipart-coffee-cafe-au-lait-caffe-americano-choice-wat-santikham-coffee-tea-coffee-thumbnail.png"
+//101 Latte 6*10**16 12*10**18 "https://w7.pngwing.com/pngs/707/144/png-transparent-latte-real-coffee-coffee-art.png"
+//102 Cappuccino 6*10**16 12*10**18 "https://ipfs.io/ipfs/QmWM7yJvHWjNLbrwZXFXNw1bB5stGbdcvh2tTs1EMWLi5H?filename=Cappuccino.png"
+
