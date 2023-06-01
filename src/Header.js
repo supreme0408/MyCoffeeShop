fa194608eca7de0cd31f4d7349ce6bcf1df973fa
Ramanuj <osupreme0408@gmail.com>
2023-06-01 17:41:07 +0530

    yet another commit

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import {React} from 'react';
+import { Center,Image} from '@chakra-ui/react';
+import './App.css'
+import logo from './logo.png';
+function Header() {
+  return (
+    <div>
+      <header>
+        <Center bg='brown' h='100px' color='white'>
+          <span className="logo">
+            <Image src={logo} height='100' width='100' margin='30px'/>
+            {/* {<img className='logo' style={{ objectFit: 'contain', maxWidth: '100%', height: '100%' }} src="https://vaya.in/recipes/wp-content/uploads/2018/05/Coffee.jpg" alt="Bit Coffee" />} */}
+          </span>
+          <h1>BIT COFFEE</h1>
+        </Center>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
